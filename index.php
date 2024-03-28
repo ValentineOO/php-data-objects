9c2cbe121dd57d6a13f04f9f6b7500343eaa022c
valentine OO <varlomxg@gmail.com>
2024-03-28 15:36:09 +0100

    fetching single post

--- index.php
+++ index.php
@@ -41,14 +41,30 @@ $pdo->setAttribute(pdo::ATTR_DEFAULT_FETCH_MODE, pdo::FETCH_OBJ);
 // FETCH MULTIPLE POSTS
 // Positional Params (?)
 
-$author = 'Alex Smith';
-$sql = 'SELECT * FROM posts WHERE author = ?';
+$author = 'Sarah Johnson';
+$is_published = true;
+$id = 1;
+
+// $sql = 'SELECT * FROM posts WHERE author = ?';
+// $stmt = $pdo->prepare($sql);
+// $stmt->execute([$author]);
+// $posts = $stmt->fetchAll();
+
+//Named Params
+// $sql = 'SELECT * FROM posts WHERE author = :author && is_published = :is_published';
+// $stmt = $pdo->prepare($sql);
+// $stmt->execute(['author' => $author, 'is_published' => $is_published]);
+// $posts = $stmt->fetchAll();
+
+
+// foreach ($posts as $post) {
+//     echo $post->title . '<br>';
+// }
+
+//FETCH SINGLE POST
+$sql = 'SELECT * FROM posts WHERE id = :id';
 $stmt = $pdo->prepare($sql);
-$stmt->execute([$author]);
-$posts = $stmt->fetchAll();
+$stmt->execute(['id' => $id]);
+$post = $stmt->fetch();
 
-// var_dump($posts);
-
-foreach ($posts as $post) {
-    echo $post->title . '<br>';
-}
+echo $post->body;
